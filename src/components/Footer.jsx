e2da51363cdd276b7b65ffd41ca7412151d3e33a
Codekacode <karem.quiroz.m@gmail.com>
2022-05-03 18:08:59 -0500

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+
+export const Footer = () => {
+    return (
+        <section>
+            <div>
+                <nav>
+                    <div><a>Shop and Learn</a></div>
+                    <div><a>Products</a></div>
+                    <div><a>For Gamers</a></div>
+                    <div><a>For Developers</a></div>
+                    <div><a>For Business</a></div>
+                    <div><a>About Aperture</a></div>
+                </nav>
+                <p>© 2022 Aperture. Todos los derechos reservados.</p>
+            </div>
+        </section>
+    )  
+}
