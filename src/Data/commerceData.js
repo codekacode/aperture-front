6b4173e2f2c675175d9a615af89a58ca0128428b
Codekacode <karem.quiroz.m@gmail.com>
2022-05-04 21:13:44 -0500

    Add fake data js

---
+++ src/Data/commerceData.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+      "id": 1,
+      "name": "The Power of HABIT",
+      "author": "Charles Duhigg",
+      "type": "hardcover",
+      "category": "business"
+    },
+    {
+      "id": 2,
+      "name": "Think and Grow Rich",
+      "author": "Napoleon Hill",
+      "type": "hardcover",
+      "category": "business"
+    }
+]
+
+export default products
--- src/utils/data.js
+++ src/utils/data.js
@@ -16,3 +16,5 @@ const products = [
         content: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s',
     }
 ]
+
+export default products
